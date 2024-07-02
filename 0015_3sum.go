3c4a0fb7b7d782adb5de05b8b8fd6418c6464ea9
XianCH <x14n911@gmail.com>
2024-07-02 19:19:59 +0800

    feat: add 0015

---
+++ 0015_3sum.go
@@ -0,0 +1,59 @@
+package leetcode
+
+import "sort"
+
+// error
+// Output
+// [[-1,0,1],[-1,2,-1],[0,1,-1]]
+// Expected
+// [[-1,-1,2],[-1,0,1]]
+
+func threesum(nums []int) [][]int {
+	result := make([][]int, 0)
+	if len(nums) < 3 {
+		return result
+	}
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			for k := j + 1; k < len(nums); k++ {
+				if nums[i]+nums[j]+nums[k] == 0 {
+					result = append(result, []int{nums[i], nums[j], nums[k]})
+				}
+			}
+		}
+	}
+	return result
+}
+
+// [-1, 0, 1, 2, -1, -4],
+// -1 -1 0 1 2 4
+// -2 -1 0 1 3 4 5
+func threeSum2(nums []int) [][]int {
+	sort.Ints(nums)
+	result := make([][]int, 0)
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left, right := i+1, len(nums)-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			} else if sum < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+	return result
+}
